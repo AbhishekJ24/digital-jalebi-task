4bb7a3447abaf1c2205dbbc51ac7b315b9629c13
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-05-25 20:01:31 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer className='p-8 text-center fixed bottom-0 w-full'>
+      &copy; 2024 Abhishek Joshi Inc. All rights reserved.
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import { React, useState } from 'react'
+const Navbar = () => {
+    const [mobileMenu, setMobileMenu] = useState(false)
+
+    return (
+        <>
+            <nav className='bg-slate-900 text-slate-200 flex justify-between items-center h-20 md:px-20 py-8 px-6 mb-10'>
+                <div className='text-2xl font-extrabold'>
+                    <a href="/"> Searchify </a>
+                </div>
+                <ul className='md:flex md:gap-10 md:visible hidden'>
+                    <li> Home </li>
+                    <li> Playground </li>
+                    <li> Docs </li>
+                </ul>
+                <button className='bg-transparent border-none invert md:hidden hover:invert-0' onClick={() => { setMobileMenu(!mobileMenu) }}>
+                        <img src="src/assets/menu.svg" alt="" />
+                    </button>
+                {mobileMenu &&
+                    <ul className='absolute z-10 right-10 top-10 bg-slate-300 rounded-xl py-5 px-4 text-slate-950'>
+                        <li className='mb-3'> Home </li>
+                        <li className='mb-3'> Playground </li>
+                        <li> Docs </li>
+                    </ul>
+                }
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
