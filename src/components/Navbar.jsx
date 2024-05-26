feccea4338a50280d1d39dada14539569b4dfdfe
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-05-26 10:00:05 +0530

    Made navbar and table responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,19 +10,19 @@ const Navbar = () => {
                 <div className='text-2xl font-extrabold'>
                     <a href="/"> Searchify </a>
                 </div>
-                <ul className='md:flex md:gap-10 md:visible hidden'>
+                <ul className='xl:flex xl:gap-10 xl:visible hidden'>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/Playground">Playground</Link></li>
                     <li><Link to="/Docs">Docs</Link></li>
                 </ul>
-                <button className='bg-transparent border-none invert md:hidden hover:invert-0' onClick={() => { setMobileMenu(!mobileMenu) }}>
+                <button className='bg-transparent border-none invert xl:hidden hover:invert-0' onClick={() => { setMobileMenu(!mobileMenu) }}>
                         <img src="src/assets/menu.svg" alt="" />
                     </button>
                 {mobileMenu &&
                     <ul className='absolute z-10 right-10 top-10 bg-slate-300 rounded-xl py-5 px-4 text-slate-950'>
-                        <li className='mb-3'> Home </li>
-                        <li className='mb-3'> Playground </li>
-                        <li> Docs </li>
+                        <li className='mb-3'> <Link to="/"> Home </Link> </li>
+                        <li className='mb-3'> <Link to="/Playground"> Playground </Link> </li>
+                        <li> <Link to="/Docs"> Docs </Link> </li>
                     </ul>
                 }
             </nav>
--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -31,33 +31,39 @@ const Playground = () => {
                     <tr>
                         <th className='w-36 h-12'>First Name</th>
                         <th className='w-36 h-12'>Last Name</th>
-                        <th className='w-36 h-12'>Gender</th>
-                        <th className='w-36 h-12'>Email</th>
-                        <th className='w-36 h-12'>Username</th>
-                        <th className='w-36 h-12'>Password</th>
-                        <th className='w-12 h-12 sm:visible hidden'> </th>
+                        <th className='w-36 h-12 hidden md:table-cell'>Gender</th>
+                        <th className='w-36 h-12 hidden lg:table-cell'>Email</th>
+                        <th className='w-36 h-12 hidden lg:table-cell'>Username</th>
+                        <th className='w-36 h-12 hidden lg:table-cell'>Password</th>
+                        <th className='w-12 h-12 xl:hidden'> </th>
                     </tr>
                 </thead>
                 <tbody className='text-slate-300 font-extralight'>
                     {data.map((user) => (
                         <tr key={user.id}>
-                            <td className='w-36 h-10'> {user.firstName} </td>
-                            <td className='w-36 h-10'> {user.lastName} </td>
-                            <td className='w-36 h-10'> {user.gender} </td>
-                            <td className='w-36 h-10'> {user.email} </td>
-                            <td className='w-36 h-10'> {user.username} </td>
-                            <td className='w-36 h-10'> {user.password} </td>
-                            <td className='w-12 h-10 sm:visible hidden'> <button onClick={() => {
-                                setseeMore(!seeMore)
-                            }
-                            } className='border-none bg-transparent mt-1 hover:invert'> <img src="src/assets/more.svg" alt="" /> </button> </td>
+                            <td className='w-36 h-10'>{user.firstName}</td>
+                            <td className='w-36 h-10'>{user.lastName}</td>
+                            <td className='w-36 h-10 hidden md:table-cell'>{user.gender}</td>
+                            <td className='w-36 h-10 hidden lg:table-cell'>{user.email}</td>
+                            <td className='w-36 h-10 hidden lg:table-cell'>{user.username}</td>
+                            <td className='w-36 h-10 hidden lg:table-cell'>{user.password}</td>
+                            <td className='w-12 h-10 xl:hidden'>
+                                <button
+                                    onClick={() => {
+                                        setseeMore(!seeMore);
+                                    }}
+                                    className='border-none bg-transparent mt-1 hover:invert'
+                                >
+                                    <img src="src/assets/more.svg" alt="" />
+                                </button>
+                            </td>
                         </tr>
                     )
                     )
                     }
                 </tbody>
             </table>}
-            <div className='text-center text-xl'>{resp ? "" : "Couldn't fetch user details"}</div>
+            <div className={resp && data ? "" : "loader"}> </div>
 
         </>
     )
